b02076259dee287e7ae8a61ec580c1123c8b712f
Krzysztof Olejniczak <thekriso@wp.pl>
2024-03-18 09:28:06 +0100

    engage

---
+++ src/components/organisms/UsersList/UsersList.js
@@ -0,0 +1,26 @@
+import React, { useState } from 'react'
+import { users as usersData } from '../../../data/users'
+import UsersListItem from '../../molecules/UsersListItem/UsersListItem'
+import { StyledList, StyledTitle, Wrapper } from './UsersList.styles'
+
+const UsersList = () => {
+	const [users, setUsers] = useState(usersData)
+
+	const deleteUser = name => {
+		const filteredUsers = users.filter(user => user.name !== name)
+		setUsers(filteredUsers)
+	}
+
+	return (
+		<Wrapper>
+			<StyledTitle>Students list</StyledTitle>
+			<StyledList>
+				{users.map(userData => (
+					<UsersListItem deleteUser={deleteUser} key={userData.name} userData={userData} />
+				))}
+			</StyledList>
+		</Wrapper>
+	)
+}
+
+export default UsersList
